8dc113a5a3a05fcceaa1e03b92ede9e2b699ea77
Alfredo Villagran Olguin <avillagran@ipn.mx>
2024-03-03 17:21:27 -0600

    Pendiente de recta, dados dos puntos

--- Ejemplo/main.c
+++ Ejemplo/main.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main(void){
-    float x1, y1, x2, y2, x3, y3;
+    float x1, y1, x2, y2, m;
 
     //1. Pide datos
     printf("Ingresa x1 ");
@@ -15,13 +15,12 @@ int main(void){
     scanf("%f",&y2);
 
     //2. Calcula formula(s)
-    x3 = x1 + x2;
-    y3 = y1 + y2;
+    m = (y2-y1) / (x2-x1);
 
     //3. Muestra resultado(s)
-    printf("  P1(%f, %f)\n",x1,y1);
-    printf("+ P2(%f, %f)\n",x2,y2);
-    printf("= P3(%f, %f)\n",x3,y3);
+    printf("P1(%f, %f)\n",x1,y1);
+    printf("P2(%f, %f)\n",x2,y2);
+    printf("m = %f\n",m);
 
     return 0;
 }
